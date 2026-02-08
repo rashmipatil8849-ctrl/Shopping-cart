7d0be0b6ac384d032e2ddbe0362e5af7b1920355
rashmi patil <rashmipatil8849@gmail.com>
2026-02-08 23:17:38 +0530

    first commit

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,21 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://shopping-cart-jn3t.onrender.com/api'
 });
 
-// Attach token to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// attach token automatically
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
