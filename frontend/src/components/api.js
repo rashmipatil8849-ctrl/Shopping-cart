361201639245c7f7fc21cc41d2409783578aa7c6
rashmi patil <rashmipatil8849@gmail.com>
2026-02-09 03:29:54 +0530

    commit

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://shopping-cart-jn3t.onrender.com/api'
+  baseURL: 'https://shopping-cart-5sp4.onrender.com/api'
 });
 
+// attach token automatically
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('token');
   if (token) {
